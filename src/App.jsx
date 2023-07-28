9f072eeac8bc9822ebf67516c1c9afcf6e7d450b
Lucas-b-o <140469984+Lucas-b-o@users.noreply.github.com>
2023-07-28 14:10:29 -0300

    1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,27 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
+import { useState } from 'react'
 
-const Content = ({ parts }) => {
+const App = () => {
+  // salve os cliques de cada botão em seu próprio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <>
-    {parts.map(part => <p>{part.name} {part.exercises}</p>)}
+      <h1>Give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+      <h1>Statistics</h1>
+      <ul>
+        <li>Good {good}</li>
+        <li>Neutral {neutral}</li>
+        <li>Bad {bad}</li>
+      </ul>
     </>
   )
 }
 
-const Total = ({ parts }) => {
-  let total = 0
-  parts.forEach(part => total = total + part.exercises)
-  return <p>Number of exercises {total}</p>
-}
-
-const App = () => {
-  const course = {
-    name: 'Desenvolvimento de aplicação Half Stack',
-    parts: [
-      {
-        name: 'Fundamentos da biblioteca React',
-        exercises: 10
-      },
-      {
-        name: 'Usando props para passar dados',
-        exercises: 7
-      },
-      {
-        name: 'Estado de um componente',
-        exercises: 14
-      }
-    ]
-  }
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
 export default App
-
-// Aqui, um array preenchido com valores inteiros é transformado em um array contendo strings de HTML usando o método map. Na parte 2 deste curso, veremos que o map é usado com frequência em Reac
