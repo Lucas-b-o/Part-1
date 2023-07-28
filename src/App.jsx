192a11d9dbb3506a1c7c79854b61c0d41ae29959
Lucas-b-o <140469984+Lucas-b-o@users.noreply.github.com>
2023-07-27 22:56:50 -0300

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from "react"
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Total = ({ total }) => {
+  return <p>Number of exercises {total}</p>
+}
 
 const App = () => {
-  const amigos = [ 'Peter', 'Maya']
+  const course = 'Desenvolvimento de aplicação Half Stack'
+  const part1 = 'Fundamentos da biblioteca React'
+  const exercises1 = 10
+  const part2 = 'Usando props para passar dados'
+  const exercises2 = 7
+  const part3 = 'Estado de um componente'
+  const exercises3 = 14
 
   return (
     <div>
-      <p>{amigos}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
