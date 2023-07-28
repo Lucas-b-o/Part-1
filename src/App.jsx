249ad222a09dd3f0a7bd331a62b1649449e9e59b
Lucas-b-o <140469984+Lucas-b-o@users.noreply.github.com>
2023-07-27 23:03:55 -0300

    Part 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,11 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
-const Content = ({
-  part1, exercises1,
-  part2, exercises2,
-  part3, exercises3
-}) => {
+const Content = ({ part, exercises }) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <p>
+      {part} {exercises}
+    </p>
   )
 }
 
@@ -38,11 +26,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
