9c6cb94ff2d456e2d91e66448d5a3418d06bf3b1
Lucas-b-o <140469984+Lucas-b-o@users.noreply.github.com>
2023-07-28 14:25:53 -0300

    1.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <ul>
+        <li>Good {good}</li>
+        <li>Neutral {neutral}</li>
+        <li>Bad {bad}</li>
+        <li>All {good + neutral + bad}</li>
+        <li>Average {(good - bad) / (good + neutral + bad)}</li>
+        <li>Positive {good * 100 / (good + neutral + bad)}</li>
+      </ul>
+    </>
+  )
+}
+
 const App = () => {
   // salve os cliques de cada botão em seu próprio estado
   const [good, setGood] = useState(0)
@@ -14,15 +30,7 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
         <button onClick={() => setBad(bad + 1)}>Bad</button>
       </div>
-      <h1>Statistics</h1>
-      <ul>
-        <li>Good {good}</li>
-        <li>Neutral {neutral}</li>
-        <li>Bad {bad}</li>
-        <li>All {good + neutral + bad}</li>
-        <li>Average {(good-bad)/(good + neutral + bad)}</li>
-        <li>Positive {good*100/(good + neutral + bad)}</li>
-      </ul>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
